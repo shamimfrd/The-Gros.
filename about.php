299150970d5ce8d7bff472937d4af17eec2d294b
shamimfrd <102221815+shamimfrd@users.noreply.github.com>
2022-06-26 17:30:49 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,157 @@
+<?php
+
+@include 'config.php';
+
+session_start();
+
+$user_id = $_SESSION['user_id'];
+
+if(!isset($user_id)){
+   header('location:login.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>about</title>
+
+   <!-- font awesome cdn link  -->
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <!-- custom css file link  -->
+   <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+   
+<?php include 'header.php'; ?>
+
+<section class="about">
+
+   <div class="row">
+
+      <div class="box">
+         <img src="images/about-img-1.png" alt="">
+         <h3>why choose us?</h3>
+         <p>Our attention to the small stuff, scheduling of timelines and keen project management that makes us stand out from the rest. We are creative, while keeping a close eye on the calendar and your budget</p>
+         <a href="contact.php" class="btn">contact us</a>
+      </div>
+
+      <div class="box">
+         <img src="images/about-img-2.png" alt="">
+         <h3>what we provide?</h3>
+         <p>We provide the best quality of <br>products to our customers<br> with the best price.</p>
+         <a href="shop.php" class="btn">our shop</a>
+      </div>
+
+   </div>
+
+</section>
+
+<section class="reviews">
+
+   <h1 class="title">clients reivews</h1>
+
+   <div class="box-container">
+
+      <div class="box">
+         <img src="images/pic-1.png" alt="">
+         <p>Lots of vegetables and lots of food available in vegetable market. Local items like apricot and etc are available.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Bikash</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-2.png" alt="">
+         <p>Atleast for me is all in one place, from some fresh fruits to dryfruits and poster flag to handicraft atleast fit me all in one place.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Pallavi</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-3.png" alt="">
+         <p>one can get everything he she needs. We can get vegetables,  fruits, fish, <br>chicken etc</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Akash</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-4.png" alt="">
+         <p>Amazing Vegetable shop and amazing service. Delivery is always fast and all fruits and veg that I receive are good quality and fresh! 100% recommended them.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Disy</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-5.png" alt="">
+         <p>Very good Quality and has the best price with lots of vegetables and fruits variety. I`m so glad, I found them. They have like more than a 100 items to offer.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Rahul</h3>
+      </div>
+
+      <div class="box">
+         <img src="images/pic-6.png" alt="">
+         <p>Lot of fresh fruits and vegetables <br>available here Also there are many shops selling <br>flowers and garlands.</p>
+         <div class="stars">
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star"></i>
+            <i class="fas fa-star-half-alt"></i>
+         </div>
+         <h3>Fiza</h3>
+      </div>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+<?php include 'footer.php'; ?>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
---
+++ footer.php
@@ -0,0 +1,41 @@
+<footer class="footer">
+
+   <section class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Home</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Shop</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> About</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Cart</a>
+         <a href="wishlist.php"> <i class="fas fa-angle-right"></i> Wishlist</a>
+         <a href="login.php"> <i class="fas fa-angle-right"></i> Login</a>
+         <a href="register.php"> <i class="fas fa-angle-right"></i> Register</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <p> <i class="fas fa-phone"></i> +918876775873 </p>
+         <p> <i class="fas fa-phone"></i> +919085576798 </p>
+         <p> <i class="fas fa-envelope"></i> shamimfrd@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> Assam, india - 782124 </p>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/officialshamim.verified"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://twitter.com/afridi_shamim"> <i class="fab fa-twitter"></i> twitter </a>
+         <a href="https://www.instagram.com/shamimfrd/?hl=en"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/in/shamim-afridi-569546107/"> <i class="fab fa-linkedin"></i> linkedin </a>
+      </div>
+
+   </section>
+
+   <p class="credit"> &copy; Copyright @ <?= date('Y'); ?> by <span>Shamim Afridi</span> | all rights reserved! </p>
+
+</footer>
